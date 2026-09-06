6e558040752a8a2432c102e945d2e26129a31134
vartcom38-collab <vartcom38@gmail.com>
2026-09-06 15:49:42 +0200

    Add homepage sprite assembler

---
+++ app/homeSprite.ts
@@ -0,0 +1,13 @@
+import p0 from './homeSpritePart0';
+import p1 from './homeSpritePart1';
+import p2 from './homeSpritePart2';
+import p3 from './homeSpritePart3';
+import p4 from './homeSpritePart4';
+import p5 from './homeSpritePart5';
+import p6 from './homeSpritePart6';
+import p7 from './homeSpritePart7';
+import p8 from './homeSpritePart8';
+
+const homeSprite = `data:image/webp;base64,${p0}${p1}${p2}${p3}${p4}${p5}${p6}${p7}${p8}`;
+
+export default homeSprite;
