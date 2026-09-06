b24fe269c30249774095506a6efe420cd04c12f0
vartcom38-collab <vartcom38@gmail.com>
2026-09-06 14:15:17 +0200

    Prepare static export for Infomaniak FTP

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  images: { unoptimized: true }
+  output: 'export',
+  images: { unoptimized: true },
+  trailingSlash: true
 };
 
 export default nextConfig;
