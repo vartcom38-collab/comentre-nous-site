98315eac841c1028ef85281ed49ea66f1b2bdfee
vartcom38-collab <vartcom38@gmail.com>
2026-09-07 16:59:30 +0200

    Add assembled hero scene asset

---
+++ app/heroScene.ts
@@ -0,0 +1,7 @@
+import heroPart0 from './heroSmallPart0';
+import heroPart1 from './heroSmallPart1';
+import heroPart2 from './heroSmallPart2';
+import heroPart3 from './heroSmallPart3';
+import heroPart4 from './heroSmallPart4';
+
+export const heroScene = `data:image/webp;base64,${heroPart0}${heroPart1}${heroPart2}${heroPart3}${heroPart4}`;
