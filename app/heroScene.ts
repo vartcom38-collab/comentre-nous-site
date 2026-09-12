84514a98ad6b55441e727f474b3ae1e3fa4233dd
vartcom38-collab <vartcom38@gmail.com>
2026-09-12 09:58:07 +0200

    Use new HD home hero image

--- app/heroScene.ts
+++ app/heroScene.ts
@@ -1,7 +1,2 @@
-import heroPart0 from './heroSmallPart0';
-import heroPart1 from './heroSmallPart1';
-import heroPart2 from './heroSmallPart2';
-import heroPart3 from './heroSmallPart3';
-import heroPart4 from './heroSmallPart4';
-
-export const heroScene = `data:image/webp;base64,${heroPart0}${heroPart1}${heroPart2}${heroPart3}${heroPart4}`;
+export const heroScene = '/home-hero-hd.png?v=20260912-hd';
+export const heroSceneMobile = '/home-hero-mobile-hd.png?v=20260912-hd';
