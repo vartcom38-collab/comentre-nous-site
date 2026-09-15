e1fc37c0ebafc4e66cfb4d54e1d42594c5fb3cc6
vartcom38-collab <vartcom38@gmail.com>
2026-09-15 09:31:56 +0200

    Fix Papeterie hero image source

--- app/papeterie-du-lien/heroImage.ts
+++ app/papeterie-du-lien/heroImage.ts
@@ -1,8 +1,5 @@
-import a from './hero-image-1';
-import b from './hero-image-2';
-import c from './hero-image-3';
-import d from './hero-image-4';
+import paper from '@/content/papeterie.json';
 
-const paperHeroImage = `data:image/webp;base64,${a}${b}${c}${d}`;
+const paperHeroImage = paper.hero.image;
 
 export default paperHeroImage;
