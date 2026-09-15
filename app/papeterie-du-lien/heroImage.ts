bf64e175c8c2771761695275d517db8b03b5c5fd
vartcom38-collab <vartcom38@gmail.com>
2026-09-15 09:27:33 +0200

    Papeterie: assemble hero image

---
+++ app/papeterie-du-lien/heroImage.ts
@@ -0,0 +1,8 @@
+import a from './hero-image-1';
+import b from './hero-image-2';
+import c from './hero-image-3';
+import d from './hero-image-4';
+
+const paperHeroImage = `data:image/webp;base64,${a}${b}${c}${d}`;
+
+export default paperHeroImage;
